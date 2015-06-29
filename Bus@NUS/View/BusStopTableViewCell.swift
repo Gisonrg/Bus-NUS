9fd5855181b72ebfd64da0ff1009dd02574d9b90
Jiang Sheng <gisonrg@gmail.com>
2015-06-29 14:08:30 +0800

    Initial commit

---
+++ Bus@NUS/View/BusStopTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  BusStopTableViewCell.swift
+//  Bus@NUS
+//
+//  Created by Jiang Sheng on 25/6/15.
+//  Copyright (c) 2015 Echx. All rights reserved.
+//
+
+import UIKit
+
+class BusStopTableViewCell : UITableViewCell {
+
+    @IBOutlet weak var label1: UILabel!
+    
+}
