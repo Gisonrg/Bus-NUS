d30004c051ef16bb9a73c0f9da7ed53cf01eea62
Jiang Sheng <gisonrg@gmail.com>
2015-07-13 00:58:59 +0800

    Basic location implementation

--- Bus@NUS/Controller/BusExplorerViewController.swift
+++ Bus@NUS/Controller/BusExplorerViewController.swift
@@ -7,15 +7,88 @@
 //
 
 import UIKit
+import CoreLocation
 import Foundation
 
-class BusExplorerViewController: UITableViewController {
+class BusExplorerViewController: UITableViewController, CLLocationManagerDelegate {
+    
+    private let LOCATION_SERVICE_DISABLED = "Location service disabled"
     
     var busStopArray:[BusStop] = [BusStop]()
+    var locationManager = CLLocationManager()
+    var userLocation = CLLocation()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpViewController()
+        
+    }
+    
+    private func setUpViewController() {
         self.title = "Bus@NUS"
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.delegate = self
+    }
+    
+    // MARK: - Location service
+    func setUpLocationService() {
+        if CLLocationManager.locationServicesEnabled() {
+            switch CLLocationManager.authorizationStatus() {
+            case CLAuthorizationStatus.AuthorizedAlways:
+                configLocationManager(startImmediately: true)
+            case .AuthorizedWhenInUse:
+                configLocationManager(startImmediately: true)
+            case .Denied:
+                NSLog(LOCATION_SERVICE_DISABLED)
+            case .NotDetermined:
+                configLocationManager(startImmediately: false)
+                self.locationManager.requestWhenInUseAuthorization()
+            case .Restricted:
+                NSLog(LOCATION_SERVICE_DISABLED)
+            }
+        }
+    }
+    
+    func configLocationManager(#startImmediately: Bool){
+        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        locationManager.distanceFilter = 100
+        if startImmediately{
+            locationManager.startUpdatingLocation()
+        }
+    }
+    
+    func locationManager(manager: CLLocationManager!, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        setUpLocationService()
+    }
+    
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        let locationArray = locations as! [CLLocation]
+        if let currentLocation = locationArray.last {
+            // update the user location
+            userLocation = currentLocation
+            println("Long: \(currentLocation.coordinate.longitude)")
+            println("Lat: \(currentLocation.coordinate.latitude)")
+            
+            // re-arrange the table by distance
+            sortBusStopByDistance()
+        }
+    }
+    
+    /**
+        Sort the busStop array by the distance to current location
+    */
+    private func sortBusStopByDistance() {
+        busStopArray.sort(compareDistanceToCurrentLocation)
+        tableView.reloadData()
+    }
+    
+    private func compareDistanceToCurrentLocation(busStop1: BusStop, busStop2: BusStop) -> Bool {
+        var busStopLocation1 = CLLocation(latitude: busStop1.latitude, longitude: busStop1.longitude)
+        var busStopLocation2 = CLLocation(latitude: busStop2.latitude, longitude: busStop2.longitude)
+        var distanceOfBusStop1 = busStopLocation1.distanceFromLocation(userLocation)
+        var distanceOfBusStop2 = busStopLocation2.distanceFromLocation(userLocation)
+        
+        return distanceOfBusStop1 < distanceOfBusStop2
     }
 
     // MARK: - Table view data source
